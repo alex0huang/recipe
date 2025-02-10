60b8e073a7e4e1690c2b4cb998bb423f4f6cf24e
alex0huang <103314926+alex0huang@users.noreply.github.com>
2025-02-10 10:21:10 +0800

    Add files via upload

--- recipe.cpp
+++ recipe.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "ingredient.h"
+#include <cassert>
 using namespace std;
 
 class dish {
@@ -15,8 +17,17 @@ class dish {
 			ingredientCount = 0;
 			
 		}
+		void addIngredient(string name, dounble quantity£¬ string unint) {
+			asscert(ingredientCount<maxingredient);
+			ingredients[maxingredient] = Ingredient(name,quantity,unit);
+		}
 		void display(){
 			cout << "Dish:" << name << endl;
 			cout <<"Amount of Calories per serving"<< calories << endl;
+			cout << "Ingredients in dish "<< endl;
+			for (int i = 0; i < ingredientCount; ++i) {
+				cout << ingredients[i].name << ":" << ingredients[i].quantity << ingredient[i].unit << endl;
+
+			}
 		}
 };
