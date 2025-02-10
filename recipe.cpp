07d95dc597addc402915d9d8a05053d0c411692e
alex0huang <103314926+alex0huang@users.noreply.github.com>
2025-02-10 09:45:17 +0800

    Add files via upload

--- recipe.cpp
+++ recipe.cpp
@@ -5,9 +5,16 @@ class dish {
 	protected: 
 		string name; 
 		double calories;
+		string name;
+		Ingredient ingredients[maxingredient];
+		double amountPerPortion[maxingredient];
+		int ingredientCount;
 		
 	public:
-		Dish(string n , double cal) : name(n) calories (cal){}
+		Dish(string n , double cal) : name(n) calories (cal){
+			ingredientCount = 0;
+			
+		}
 		void display(){
 			cout << "Dish:" << name << endl;
 			cout <<"Amount of Calories per serving"<< calories << endl;
