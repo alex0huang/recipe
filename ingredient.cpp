6cb1ae2ceb97341892ba49f68ed2a94548280cab
alex0huang <103314926+alex0huang@users.noreply.github.com>
2025-03-06 12:30:12 +0800

    Add files via upload

--- ingredient.cpp
+++ ingredient.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "ingredient.h"
 
-using namespace std;
+using namespace std {
 
-class Ingredient {
-	
+    void show_ingredient() {
+        cout << "ingredient in 100 gram :" << ingred_name << "Calories in 100 gram: " << caloriess_per_100gram << endl;
+        cout << "protein amount for in 100:" << ingred_calories << "gram" << endl;
+    }
 
-};
+    int get_
+
+
+
+}
--- ingredient.h
+++ ingredient.h
@@ -4,26 +4,43 @@
 
 
 class Ingredient{
-   public:
+   private:
 		string ingred_name;
-		double stock_quantity;
-		bool perishable;
-        int calories;
-	
+		//double stock_quantity;
+		//bool perishable;
+        int ingred_protein;
+        int ingred_calories;
+public:
 		Ingredient(){
 			ingred_name = "unknown";
-			stock_quantity = 0.0;
-            calories = 0;
+			//stock_quantity = 0.0;
+            calories_per_100gram = 0;
+            ingred_protein = 0;
 		}
+        Ingredient(string i_name, int cpg, int i_protein) {
+            ingre_name = i_name;
+            calories_per_100gram = cpg;
+            ingred_protein = i_protein;
+        }
 
-		// Viewers
-
-        // Print out each ingredient + quanitity
-		void display_stock();
-        void display_pantry();
-        
+        void show_ingredient();
+        int get_calories();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
         // Editor
         // Add stock to an ingredient
         void add_ingredient_stock(double addend);
@@ -36,7 +53,7 @@ class Ingredient{
         // restocks each ingredient to 500 grams + if not cleared ingredient
         // Then changes to 500 grams
         void restock();
-    
+        */
 
 
 
